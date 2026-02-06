2b993c3f64520c93df45a51afff42e99c175d84b
Christian <codirrl@gmail.com>
2026-02-05 18:40:37 -0600

    Commit Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { Logo, Nav, NumResults, Search } from "./components/Nav";
+import { Box } from "./components/Box";
+import { MovieList } from "./components/Movie";
+import { WatchedMoviesContainer, WatchedMoviesList, WatchedSummary } from
+"./components/WatchedMovies";
+import { useFetchMovies } from "./hooks/useFetchMovies";
+import { MovieDetails } from "./components/MovieDetails";
+import { useLocalStorageState } from "./hooks/useLocalStorageState";
 
-function App() {
-  const [count, setCount] = useState(0)
+// componente principal
+export default function App() {
+    
+    // búsqueda de pelis
+    const [query, setQuery] = useState("");
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // obtiene pelis
+    const {movies, isLoading, error} = useFetchMovies(query);
+
+    // pelis vistas
+    const [watched, setWatched] = useLocalStorageState([], "watched");
+
+    function handleDeleteWatched(id) {
+    setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
+    }      
+
+    // estado peli seleccionada
+    const [selectedId, setSelectedId] = useState(null);
+
+    /**
+    * Maneja la selección de una película.
+    * @param {string} id - ID de la película seleccionada.
+    */
+
+    function handleSelectMovie(id) {
+        setSelectedId((selectedId) => (id === selectedId ? null : id));
+    }
+
+    // cierra detalles de la peli
+    function handleCloseMovie() {
+        setSelectedId(null);
+    }
+
+    /**
+    * Agrega una película a la lista de vistas.
+    * @param {Object} movie - Película a agregar.
+    */
+
+    function handleAddWatched(movie) {
+        const isAlreadyWatched = watched.some((m) => m.imdbID === movie.imdbID);
+
+        if (!isAlreadyWatched) {
+            setWatched((watched) => [...watched, movie]);
+            handleCloseMovie();
+        } else {
+        // si ya existe, simplemente cerramos el detalle
+        handleCloseMovie();
+    }
+    }
+
+    return (
+        <>
+            <Nav>
+                <Logo />
+                <Search query={query} setQuery={setQuery} />
+                <NumResults movies={movies} />
+            </Nav>
+            <main className="main">
+                <Box>
+                    {isLoading && <p className="loader">Cargando...</p>}
+                    {!isLoading && !error && (
+                        <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+                    )}
+                    {error && <p className="error">⛔ {error}</p>}
+                    <MovieList movies={movies} onSelectMovie={handleSelectMovie} />
+                </Box>
+
+                <Box>
+                    <WatchedMoviesContainer>
+                        {selectedId ? (
+                            <MovieDetails
+                                selectedId={selectedId}
+                                onCloseMovie={handleCloseMovie}
+                                onAddWatched={handleAddWatched}
+                                watched={watched}
+                            />
+                        ) : (
+                            <>
+                                <WatchedSummary watched={watched} />
+                                <WatchedMoviesList watched={watched} 
+                                onDeleteWatched={handleDeleteWatched}/>
+                            </>
+                        )}
+                    </WatchedMoviesContainer>
+                </Box>
+            </main>
+        </>
+    );
 }
-
-export default App
